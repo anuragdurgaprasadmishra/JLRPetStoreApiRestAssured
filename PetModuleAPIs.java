cd16257c8a40e14c9d4c6ef4e0872867a72cc38e
anuragdurgaprasadmishra <9028anurag@gmail.com>
2025-06-10 15:11:19 +0530

    JLR Rest Assured Test Cases

---
+++ PetModuleAPIs.java
@@ -0,0 +1,117 @@
+package RestAssuredAPITestCases;
+
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PetModuleAPIs {
+	
+	//Test Case1: To Validate that the request is successfully processed or not with Valid URL
+	
+	@Test
+	public void getPetStatus_TC_001()
+	{
+		RestAssured.baseURI = "https://petstore.swagger.io";
+	
+		given().log().all()
+			.when().log().all()
+				.get("/v2/pet/findByStatus")
+					.then().log().all()
+						.assertThat()
+							.statusCode(200);
+	}
+	
+	
+	//Test Case2: To Validate the request if the URL is Invalid and Application should throw 401 Error
+	
+	@Test
+	public void getPetStatus_TC_002()
+	{
+		RestAssured.baseURI = "https://petstore.swagger.io";
+	
+		given().log().all()
+			.when().log().all()
+				.get("/v2/pet/findByStatus01")
+					.then().log().all()
+						.assertThat()
+							.statusCode(404);
+	}
+	
+	
+	//Test Case3: To Validate the request with Status = Available
+	// Implemented with the help of QueryParam
+	
+	@Test
+	public void getPetStatus_TC_003()
+	{
+		RestAssured.baseURI = "https://petstore.swagger.io";
+	
+		given().log().all()
+			.queryParam("status", "available")
+				.when().log().all()
+					.get("/v2/pet/findByStatus")
+						.then().log().all()
+							.assertThat()
+								.statusCode(200);
+	}
+	
+	// Test Case4: To Validate the request with Status = pending
+	// Implementation with the help of HashMap
+	
+		@Test
+		public void getPetStatus_TC_004()
+		{
+			RestAssured.baseURI = "https://petstore.swagger.io";
+			
+			Map<String, String> queryMap = new HashMap<String, String>();
+			queryMap.put("status", "pending");
+			
+			given().log().all()
+				.when().log().all()
+					.get("/v2/pet/findByStatus")
+						.then().log().all()
+							.assertThat()
+								.statusCode(200);
+		}
+		
+		
+	// Test Case5: To Validate the request with Status = sold
+	// Implemented with the help of QueryParam
+	
+		@Test
+		public void getPetStatus_TC_005()
+		{
+			RestAssured.baseURI = "https://petstore.swagger.io";
+		
+			given().log().all()
+				.queryParam("status", "sold")
+					.when().log().all()
+						.get("/v2/pet/findByStatus")
+							.then().log().all()
+								.assertThat()
+									.statusCode(200);
+		}	
+		
+		
+	//Test Case6: To Validate that the application is fetch the Pet Details based on ID
+	
+		@Test
+		public void findPetByID_TC_006()
+		{
+			RestAssured.baseURI = "https://petstore.swagger.io";
+		
+			given().log().all()
+				.when().log().all()
+					.get("/v2/pet/2580") // Used Pet ID --> 2580
+						.then().log().all()
+							.assertThat()
+								.statusCode(200);
+		}	
+	
+
+}
